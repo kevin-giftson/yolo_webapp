5f37056a3ffcebac0e6c77cc7f0a67fbbb5c3537
Kevin Giftson <159572469+kevin-giftson@users.noreply.github.com>
2025-02-04 23:29:42 +0530

    Add files via upload

---
+++ YOLO_Image.py
@@ -0,0 +1,36 @@
+from ultralytics import YOLO
+import cv2
+import math
+
+def image_detection(path_x):
+    img = cv2.imread(image_path)
+
+
+    model = YOLO("../YOLO-Weights/shape.pt")
+    classNames = ["circle","hexagon","rectangle","square","triangle"
+                  ]
+
+    while True:
+        success, img = cap.read()
+        results=model(img,stream=True)
+        for r in results:
+            boxes = r.boxes
+            for box in boxes:
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+                conf = math.ceil((box.conf[0] * 100)) / 100
+                cls = int(box.cls[0])
+                class_name = classNames[cls]
+                label = f'{class_name} {conf}'
+                t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+                c2 = x1 + t_size[0], y1 - t_size[1] - 3
+                cv2.rectangle(img, (x1, y1), c2, [255, 0, 255], -1, cv2.LINE_AA)  # filled
+                cv2.putText(img, label, (x1, y1 - 2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)
+
+        yield img
+         #out.write(img)
+        #cv2.imshow("image", img)
+        #if cv2.waitKey(1) & 0xFF==ord('1'):
+            #break
+    #out.release()
+cv2.destroyAllWindows()
